19a566e4eb23cab9c3805fcf1ceb264dfb7c2fe2
lglatimer <lgl78.online@gmail.com>
2014-11-25 17:37:39 -0500

    Added in follow all rules

--- Demo_proj1/Demo_proj1/items.py
+++ Demo_proj1/Demo_proj1/items.py
@@ -12,4 +12,5 @@ class DemoProj1Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     link = scrapy.Field()
-
+    depth = scrapy.Field()
+    referring_url = scrapy.Field()
--- Demo_proj1/Demo_proj1/spiders/DemoSpider.py
+++ Demo_proj1/Demo_proj1/spiders/DemoSpider.py
@@ -1,14 +1,21 @@
 import scrapy
-
 from Demo_proj1.items import DemoProj1Item
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+
 
 class DemoProj1Spider(scrapy.Spider):
 	name = "Tutorial"
-	allowed_domains =["punchng.com"]
+#	allowed_domains =["punchng.com"]
 	start_urls = ["http://www.punchng.com"]
+	rules = (Rule (SgmlLinkExtractor(allow=("", ),),
+                callback="parse_items",  follow= True),
+    )
 
-	def parse(self, response):
+	def parse_items(self, response):
 		for url in response.xpath('//*'):
 			item = DemoProj1Item()
 			item['link'] = url.xpath('a/@href').extract()
+			referring_url = response.request.headers.get('Referer', None)
+			item['referring_url'] = referring_url
+			item['depth'] = response.meta['depth']
 			yield item
